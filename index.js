0f445f29bb276da4158895997d6d276aac812e30
Jeannito <jeanb2r@gmail.com>
2022-05-23 22:28:53 +0200

    [main] init sequelize

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+// Puppeteer
 const browserObject = require('./browser');
 const scraperController = require('./pageController');
+// Sequelize
+const initModels = require("./src/models/init-models.js");
+const { Sequelize } = require('sequelize');
 
-//Start the browser and create a browser instance
+require('dotenv').config();
+
+// INIT SEQUELIZE
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql'
+});
+
+sequelize
+    .authenticate()
+    .then(
+        () => console.log('Connection has been established successfully.'),
+        error => console.error('Unable to connect to the database:', error)
+    );
+
+// INIT SEQUELIZE MODELS
+initModels(sequelize);
+
+// Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
 
 // Pass the browser instance to the scraper controller
