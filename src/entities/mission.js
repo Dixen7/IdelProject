7f312838ad4cc26eb0bc2ed1edae92159b78ebfa
Jeannito <jeanb2r@gmail.com>
2022-05-23 21:08:49 +0200

    [main] create mission entities

---
+++ src/entities/mission.js
@@ -0,0 +1,20 @@
+class Mission {
+
+    constructor (description, gender, location, type, start_date, end_date, retrocession, skills, working_days, need_vehicles, establishment_type, source_link, necessary_equipment) {
+        this.description = description;
+        this.gender = gender;
+        this.location = location;
+        this.type = type;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.retrocession = retrocession;
+        this.skills = skills;
+        this.working_days = working_days;
+        this.need_vehicles = need_vehicles;
+        this.establishment_type = establishment_type;
+        this.source_link = source_link;
+        this.necessary_equipment = necessary_equipment;
+    }
+
+}
+export default AdditionalData;
