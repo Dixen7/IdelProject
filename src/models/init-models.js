49df362778a55a29bb091e763a9139bd230cd48b
Jeannito <jeanb2r@gmail.com>
2022-05-23 21:39:21 +0200

    [main] add mission on model init

--- src/models/init-models.js
+++ src/models/init-models.js
@@ -1,14 +1,18 @@
 const DataTypes = require("sequelize").DataTypes;
 const _user = require("./user");
+const _mission = require("./mission");
 
 function initModels(sequelize) {
   const user = _user(sequelize, DataTypes);
+  const mission = _mission(sequelize, DataTypes);
 
 
   return {
     user,
+    mission
   };
 }
+
 module.exports = initModels;
 module.exports.initModels = initModels;
 module.exports.default = initModels;
