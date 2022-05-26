a5ec4fe9918d2b9d7310cfde54ca7b8e9c2f9c03
Dixen7 <balestrinoclement7@gmail.com>
2022-05-26 14:44:40 +0200

    uncomment index.js

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 const browserObject = require('./src/scraping/browser')
 const scraperController = require('./src/scraping/pageController');
 
-// // Sequelize
-// const initModels = require("./src/models/init-models.js");
-// const { Sequelize } = require('sequelize');
+// Sequelize
+const initModels = require("./src/models/init-models.js");
+const { Sequelize } = require('sequelize');
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// // INIT SEQUELIZE
-// const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-//     host: process.env.MYSQL_HOST,
-//     port: process.env.MYSQL_PORT,
-//     dialect: 'mysql'
-// });
+// INIT SEQUELIZE
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql'
+});
 
-// sequelize
-//     .authenticate()
-//     .then(
-//         () => console.log('Connection has been established successfully.'),
-//         error => console.error('Unable to connect to the database:', error)
-//     );
+sequelize
+    .authenticate()
+    .then(
+        () => console.log('Connection has been established successfully.'),
+        error => console.error('Unable to connect to the database:', error)
+    );
 
-// // INIT SEQUELIZE MODELS
-// initModels(sequelize);
+// INIT SEQUELIZE MODELS
+initModels(sequelize);
 
 // Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
