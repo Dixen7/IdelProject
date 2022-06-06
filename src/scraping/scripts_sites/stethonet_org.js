4183db60ba0b98e653c88b8b53c8b69ff10a39ec
Dixen7 <balestrinoclement7@gmail.com>
2022-06-06 21:09:57 +0200

    add mission type to stethonet

--- src/scraping/scripts_sites/stethonet_org.js
+++ src/scraping/scripts_sites/stethonet_org.js
@@ -5,11 +5,8 @@ const scraperObject = {
     async scraper(browser){
 
         let page = await browser.newPage();
-
 		console.log(`Navigating to ${this.url}...`);
-
         await page.goto(this.url, {waitUntil: "networkidle2"})
-
 
         let response = async function scrapSite() {
 
@@ -20,19 +17,22 @@ const scraperObject = {
                     let mission = {};
     
                     if (missionText) {
-                        mission.description = missionText.innerText.trim().replace("\n", " ");
-                        mission.location = missionText.previousElementSibling.innerText.trim().split(' ')[2];
+                        mission.description = missionText.innerText ? mission.innerText.trim().replace("\n", " ") : "";
+                        mission.location = missionText.previousElementSibling.innerText ? missionText.previousElementSibling.innerText.trim().split(' ')[2]: "";
+                        mission.source_link = this.url;
+                        if (["remplacant", "remplacante", "remplacant(e)", "remplacement", "remplaçant(e)", "remplaçant", , "remplaçante"].some(el => missionText.innerText.toLowerCase().trim().includes(el))) {
+                            mission.type = "remplacement";
+                        } else {
+                            mission.type = "";
+                        }
                     }
-                    
                     return mission;
                 });
             })
-
             console.log(missions)
         }
 
         await response();
-        await page.close();
         await browser.close();
 
     } 
