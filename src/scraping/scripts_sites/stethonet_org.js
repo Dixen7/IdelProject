203c688e0de6bfbc1f8ab334a4f398c33b6e00c8
Dixen7 <balestrinoclement7@gmail.com>
2022-07-02 13:24:29 +0200

    add field in stethonet

--- src/scraping/scripts_sites/stethonet_org.js
+++ src/scraping/scripts_sites/stethonet_org.js
@@ -13,14 +13,45 @@ const scraperObject = {
             let missions = await page.evaluate(() => {
                 return Array.from(document.querySelectorAll("#fondnews"))
                 .map(missionText => {
+
                     let mission = {};
+                    let user = {}
                     if (missionText) {
-                        mission.description = missionText && missionText.innerText ? missionText.innerText.trim().replace("\n", " ") : "";
-                        mission.location = missionText.previousElementSibling.innerText ? missionText.previousElementSibling.innerText.trim().split(' ')[2]: "";
-                        mission.
+
+                        // Normalize description input
+                        let descriptionInput = missionText && missionText.innerText ? missionText.innerText.trim().replace(/\n/g," ") : "";
+
+                        // Split description in array
+                        const arrayDescription = descriptionInput.split(' ');
+
+                        // User phone
+                        mission.phone = arrayDescription.map(e => {
+                            return e.includes('06') ? e : null
+                        }).filter(el => el != null);
+
+                        // Publication date
+                        mission.date = arrayDescription[0];
+
+                        // Mission postalcode
+                        mission.zip = arrayDescription.pop();
+
+                        // Mission description
+                        mission.description = descriptionInput;
+
+                        // Mission location (to parse departement and create mission for all departement)
+                        mission.region = missionText.previousElementSibling.innerText ? missionText.previousElementSibling.innerText.trim().split(' ')[2]: "";
+                        mission.departementArray = missionText.previousElementSibling.innerText ? missionText.previousElementSibling.innerText.trim().replace(/[^0-9]/g).split(' ')[2]: "";
+
+                        // Mission mail
+                        mission.creator_id = arrayDescription.map(e => {
+                            return e.includes('@') ? e : null
+                        }).filter(el => el != null)[0];
+
+                        // Mission URL source
                         mission.source_link = this.url;
-                        if (["remplacant", "remplacante", "remplacant(e)", "remplacement", "remplaçant(e)", "remplaçant", , "remplaçante"]
-                            .some(el => missionText.innerText.toLowerCase().includes(el))) {
+
+                        // Mission type
+                        if (["remplacant", "remplacante", "remplacant(e)", "remplacement", "remplaçant(e)", "remplaçant", , "remplaçante"].some(el => missionText.innerText.toLowerCase().includes(el))) {
                             mission.type = "remplacement";
                         }
                     }
